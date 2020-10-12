fd20760980bf6f5ce926ba30fa4c66e4b550ab1e
Rizwan Hashmi <mri.hashmi@gmail.com>
2020-10-12 16:02:37 -0400

    Update App.js
    Added subjects

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ function App() {
   return  (
     <div className="App">
 
-     <h1>Hello World from Rizwan</h1>
-      <Student totalA="40" totalB="1"/>
-      
+     <h1>Stastics of score card</h1>
+     
+     <br/><hr/>
+     <h1>Science</h1>
+      <Student totalA="40" totalB="12" totalC="4" totalF="1"/>
+
+      <br/><hr/>
+      <h1>Maths</h1>
+      <Student totalA="30" totalB="10" totalC="0" totalF="6"/>
+
+      <br/><hr/>
+      <h1>Geography</h1>
+      <Student totalA="35" totalB="1" totalC="3" totalF="2"/>
 
 
 
