34d2f82799c7645a8db643dbfe307c9b18b1f6fd
Rizwan Hashmi <mri.hashmi@gmail.com>
2020-10-12 16:02:26 -0400

    Update student.js
    Added grades

--- src/student.js
+++ src/student.js
@@ -5,6 +5,8 @@ function Student (props){
         <div>
             <h1> Total number of student with A: {props.totalA}</h1>
             <h1> Total number of student with B: {props.totalB}</h1>
+            <h1> Total number of student with C: {props.totalC}</h1>
+            <h1> Total number of student Failed: {props.totalF}</h1>
         </div>
     )
 }
